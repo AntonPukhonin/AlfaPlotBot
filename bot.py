f66f609cc888001defb383673926de53393c9403
xzibit68 <afanmatvey93@gmail.com>
2015-12-04 18:21:21 +0300

    Modify bot.py. Adding images

--- bot.py
+++ bot.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
+# coding: utf-8
 import time
 import telebot
 from telebot import types
 import config
 import os
-import psycopg2
 
 bot = telebot.TeleBot(config.token)
 
@@ -17,17 +16,30 @@ def handle_start(message):
         bot.register_next_step_handler(msg, on_section_click)
 
 def on_section_click(message):
-        if message.text == 'Курсы':
+        if message.text == u'Курсы':
                 markup = types.ReplyKeyboardMarkup()
                 markup.row('Назад')
                 markup.row('USD/RUB', 'EUR/RUB')
                 msg = bot.send_message(message.chat.id, "Выберите актив:", reply_markup=markup)
                 bot.register_next_step_handler(msg, on_currency_click)
+        elif message.text == u'Пифы':
+                msg = bot.send_message(message.chat.id, "Пока не готово(")
+                bot.register_next_step_handler(msg, on_section_click)
+        elif message.text == u'Траты':
+                msg = bot.send_message(message.chat.id, "Пока не готово(")
+                bot.register_next_step_handler(msg, on_section_click)
 
 def on_currency_click(message):
-        if message.text == 'Назад':
-                handle_start(message)
+        if message.text == u'Назад':
+            handle_start(message)
+        elif message.text == u'USD/RUB':
+            msg = bot.send_photo(message.chat.id, open('kitten.jpg', 'rb'))
+            bot.register_next_step_handler(msg, on_currency_click)
+        elif message.text == u'EUR/RUB':
+            msg = bot.send_photo(message.chat.id, open('rooster.jpg', 'rb'))
+            bot.register_next_step_handler(msg, on_currency_click)
 
+bot.polling()
 
 if __name__ == '__main__':
         os.environ['http_proxy'] = 'http://datahub-20:8888'
@@ -35,4 +47,3 @@ if __name__ == '__main__':
         bot.polling(none_stop=True)
         os.environ['http_proxy'] = 'http://localhost:3128/'
         os.environ['https_proxy'] = 'http://localhost:3128/'
-
