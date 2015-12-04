c95f996912b8afbb0111670b8693bd760dd09a28
Leonid Kuligin <lkuligin@alfabank.ru>
2015-12-04 23:17:37 +0400

    chart

---
+++ rawdata/chart.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+# coding: utf8
+
+import logging
+import traceback
+from datetime import datetime, date, timedelta
+from utils import get_connection
+PATH ="./config.json"
+CONN_NAME = "RBPROD"
+
+
+def get_cities_names():
+	#return dict {name: [id]}
+	conn = get_connection(CONN_NAME, PATH)
+	cur = conn.cursor()
+	##cur.execute("SELECT asset_id, symb, name, conn_name FROM exprm.assets")
+	try:
+		cur.execute("SELECT REGION_, (case when REGION_ = 1 then 'Moscow' when REGION_=2 then 'SPB' else 'Perm' end) city FROM expm_balances GROUP BY REGION_")
+		conn.commit()
+		assets_list = {}
+		for row in cur:
+			assets_list[row[1]] = [row[0]]
+	except:
+		logging.error(traceback.format_exc())
+	conn.close()
+	return assets_list
+
+def get_ages(city_id):
+	#return dict {name: [id]}
+	conn = get_connection(CONN_NAME, PATH)
+	cur = conn.cursor()
+	##cur.execute("SELECT asset_id, symb, name, conn_name FROM exprm.assets")
+	try:
+		cur.execute("SELECT age FROM expm_balances where AGE<80 and AGE > 10 and region_ = " + str(city_id) + "GROUP BY AGE")
+		conn.commit()
+		ages_list = []
+		for row in cur:
+			assets_list.append([0])
+	except:
+		logging.error(traceback.format_exc())
+	conn.close()
+	return ages_list
+
+
+def get_chart(city_id, age_id):
+	conn = get_connection(CONN_NAME, PATH)
+	cur = conn.cursor()
+	cur.execute("SELECT * FROM EXPM_BALANCES WHERE AGE = " + str(age_id) + "AND REGION_ = " + str(city_id) + " ORDER BY 4 desc")
+	conn.commit()
+	res = {}
+	for row in cur:
+		res[row[2]] = row[3]
+	conn.close()
+	return res
+
+
+
