5053dc9019645e339fcbea5e0ff5941a8f2d045a
xzibit68 <afanmatvey93@gmail.com>
2015-12-05 08:01:05 +0300

    Change bot ui:

--- bot.py
+++ bot.py
@@ -28,39 +28,56 @@ def command_help(message):
 @bot.message_handler(commands=['start'])
 def handle_start(message):
         markup = types.ReplyKeyboardMarkup(selective=False)
-        markup.row('Траты')
-        markup.row('Курсы')
+        markup.row('Финансы')
+        markup.row('Курсы валют')
+        markup.row('Акции')
         msg = bot.send_message(message.chat.id, "Выберите раздел:", reply_markup=markup)
         bot.register_next_step_handler(msg, on_section_click)
 
 def on_section_click(message):
-        if message.text == u'Курсы':
-                markup = types.ReplyKeyboardMarkup(selective=False)
-                markup.row('Назад')
-                markup.row('SBER за неделю', 'EUR/RUB за неделю')
-                markup.row('SBER за год', 'EUR/RUB за год')
-                msg = bot.send_message(message.chat.id, "Выберите актив:", reply_markup=markup)
-                bot.register_next_step_handler(msg, on_currency_click)
-        elif message.text == u'Траты':
-                categories, expen = mockChart.get_chart(0, 0)
-                user = magicNumber.createMagicData(expen, categories)
+        if message.text == u'Курсы валют':
+            markup = types.ReplyKeyboardMarkup(selective=False)
+            markup.row('Назад')
+            markup.row('ESD/RUB за неделю', 'EUR/RUB за неделю')
+            markup.row('ESD/RUB за год', 'EUR/RUB за год')
+            msg = bot.send_message(message.chat.id, "Выберите актив:", reply_markup=markup)
+            bot.register_next_step_handler(msg, on_currency_click)
+        elif message.text == u'Финансы':
+            categories, expen = mockChart.get_chart(0, 0)
+            user = magicNumber.createMagicData(expen, categories)
 
-                expenses.createChart(categories, expen, user)
+            expenses.createChart(categories, expen, user)
 
-                msg = bot.send_photo(message.chat.id, open('filename.png', 'rb'))
-                bot.register_next_step_handler(msg, on_section_click)
+            msg = bot.send_photo(message.chat.id, open('filename.png', 'rb'))
+            bot.register_next_step_handler(msg, on_section_click)
+        elif message.text == u'Акции':
+            markup = types.ReplyKeyboardMarkup(selective=False)
+            markup.row('Назад')
+            markup.row('SBER за неделю', 'SBER за неделю')
+            msg = bot.send_message(message.chat.id, "Выберите период:", reply_markup=markup)
+            bot.register_next_step_handler(msg, on_stock_click)
+
 
 def on_currency_click(message):
         if message.text == u'Назад':
             handle_start(message)
-        elif message.text == u'SBER за неделю':
-            createAndShowChart(message, 1, True, "SBER")
         elif message.text == u'EUR/RUB за неделю':
             createAndShowChart(message, 2, True, "EUR/RUB")
-        elif message.text == u'SBER за год':
-            createAndShowChart(message, 1, False, "SBER")
         elif message.text == u'EUR/RUB за год':
             createAndShowChart(message, 2, False, "EUR/RUB")
+        elif message.text == u'ESD/RUB за неделю':
+            createAndShowChart(message, 1, True, "EUR/RUB")
+        elif message.text == u'ESD/RUB за год':
+            createAndShowChart(message, 1, False, "EUR/RUB")
+
+def on_stock_click(message):
+        if message.text == u'Назад':
+            handle_start(message)
+        elif message.text == u'SBER за неделю':
+            createAndShowChart(message, 1, True, "SBER")
+        elif message.text == u'SBER за год':
+            createAndShowChart(message, 1, False, "SBER")
+
 
 def createAndShowChart(message, index, isWeek, title):
     res = asset.get_asset_quotes(index)
